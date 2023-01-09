3d341662eb59ef90ebe9f77f3513805e9fcf4aa8
YunSef <sebblonski77@gmail.com>
2023-01-09 12:36:33 +0100

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstExercice';
+  title = 'Bienvenue sur le site de Seb';
 }
